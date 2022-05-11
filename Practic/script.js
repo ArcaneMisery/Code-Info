d0a89e606da4c263f71c80c0f8abdb65c4f5cd33
Vladimir <vova.kudinov.2017@inbox.ru>
2022-05-11 13:53:17 +0300

    11.05.

--- Practic/script.js
+++ Practic/script.js
@@ -432,4 +432,30 @@ arr.push(function() {
   console.log( this );
 })
 
-arr[2]();
+arr[2]();  
+
+
+function loadJson(url) {
+   return fetch(url)
+     .then(response => {
+       if (response.status == 200) {
+         return response.json();
+       } else {
+         throw new Error(response.status);
+       }
+     })
+ }
+ loadJson('https://dsadsa')
+ async function loadJson(url) {
+   let response = await fetch(url);
+   if (response.status == 200) {
+      let json = await response.json()
+      return json()
+   }
+
+   throw new Error(response.status)
+}
+
+loadJson('dsadsa')
+.catch(alert)
+ 
